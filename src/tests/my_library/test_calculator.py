2f7797feadae8a183e924630ed3d864dd039c706
shige1000 <qqq.558.vvv@gmail.com>
2025-03-21 00:09:10 +0900

    [fix]

--- src/tests/my_library/test_calculator.py
+++ src/tests/my_library/test_calculator.py
@@ -1,5 +1,11 @@
+import os
+import sys
 import unittest
-from apps.my_library.calculator import add, subtract, multiply, divide
+
+# テスト実行時のパスを調整
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
+from src.apps.my_library.calculator import add, subtract, multiply, divide
 
 
 class TestCalculator(unittest.TestCase):

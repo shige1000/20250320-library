1d59f32a821edafd58036f7c8fab1fab8ea55e44
shige1000 <qqq.558.vvv@gmail.com>
2025-03-23 16:43:48 +0900

    [fix]testコミット

---
+++ src/my_library/apps/csv/service/__init__.py
@@ -0,0 +1,8 @@
+# my_library.apps.csv.service パッケージ
+"""
+CSVサービスに関するモジュールを格納するパッケージ
+"""
+
+from .csv_service import CsvService
+
+__all__ = ["CsvService"]
---
+++ src/my_library/utils/__init__.py
@@ -0,0 +1,8 @@
+# my_library.utils パッケージ
+"""
+ユーティリティモジュールを格納するパッケージ
+"""
+
+from .singleton import Singleton
+
+__all__ = ["Singleton"]

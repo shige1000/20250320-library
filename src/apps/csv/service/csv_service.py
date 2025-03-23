e51d4f1193b211c52fd37a61326f269777102aad
shige1000 <qqq.558.vvv@gmail.com>
2025-03-23 17:17:10 +0900

    [fix]現状コミット

--- src/apps/csv/service/csv_service.py
+++ src/apps/csv/service/csv_service.py
@@ -1,10 +1,14 @@
 import csv
 from utils.singleton import Singleton
+from apps.csv.service.csv_service_interface import CsvServiceInterface
 from typing import List
-from .csv_service_interface import CsvServiceInterface
 
 
 class CsvService(Singleton, CsvServiceInterface):
+    def __init__(self):
+        # 初期化処理
+        pass
+
     def create_csv(self, data: List[list], csv_path: str):
         """
         CSVファイルを作成するメソッド
@@ -13,9 +17,9 @@ class CsvService(Singleton, CsvServiceInterface):
             data (List[list]): CSVファイルに書き込むデータ
             csv_path (str): 作成するCSVファイルのパス
         """
-        with open(csv_path, "w") as f:
+        with open(csv_path, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(data)
 
     def test(self):
-        print("test")
+        print("CSVService is working!")
--- src/apps/csv/service/csv_service_interface.py
+++ src/apps/csv/service/csv_service_interface.py
@@ -6,3 +6,7 @@ class CsvServiceInterface(ABC):
     @abstractmethod
     def create_csv(self, data: List[list], csv_path: str):
         pass
+
+    @abstractmethod
+    def test(self):
+        pass
